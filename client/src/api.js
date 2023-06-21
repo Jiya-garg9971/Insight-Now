514311944324de1ea016a17e45803fa0af738dbe
Jiya-garg9971 <9971jiyagarg@gmail.com>
2023-06-21 21:31:32 +0530

    added nodemon

--- client/src/api.js
+++ client/src/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios'; //use to fetch data
 const getNews=async()=>{
-    const url="http://localhost:4800";
+    const url="https://insightnow.onrender.com";
     try{
         return await axios.get(`${url}/news`);
     }
     catch(e){
-        console.log("e");
+        console.log("error",e);
     }
 }
 export default getNews;
